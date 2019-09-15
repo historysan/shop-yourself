e516e1a2ebc22b6c6b1ff5dc4bba346790585f48
sandro <sandrokakashvili@gmail.com>
2019-09-15 22:51:12 +0400

    feat: cart function

--- src/app/components/Button/Button.jsx
+++ src/app/components/Button/Button.jsx
@@ -3,9 +3,9 @@
 import React from 'react'
 import './Button.scss'
 
-const Button = ({ children, isGoogleSignIn, ...otherProps }) => (
-  <button className={`${isGoogleSignIn ? 'google-sign-in' : ''} custom-button`} {...otherProps}>
-    {children}
+const Button = ({ children, isGoogleSignIn, inverted, ...otherProps }) => (
+  <button className={`${inverted ? 'inverted' : ''}${isGoogleSignIn ? 'google-sign-in' : ''} custom-button`} {...otherProps}>
+    {children.toUpperCase()}
   </button>
 )
 
--- src/app/redux/cart/cartActions.js
+++ src/app/redux/cart/cartActions.js
@@ -3,3 +3,8 @@ import { cartActionTypes } from './cartTypes'
 export const toggleCartHidden = () => ({
   type: cartActionTypes.TOGGLE_CART_HIDDEN
 })
+
+export const addItem = payload => ({
+  type: cartActionTypes.ADD_ITEM,
+  payload
+})
